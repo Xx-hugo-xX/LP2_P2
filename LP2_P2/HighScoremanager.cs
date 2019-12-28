3ff69dcc98a82a88f6b7ddc0c0c93f3cc2e2ed7f
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-12-28 22:58:27 +0000

    Add fully functioning 'HighScoreManager' class and to be improved 'MainMenu' class

---
+++ LP2_P2/HighScoremanager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LP2_P2
+{
+    class HighScoreManager
+    {
+        private readonly string appName;
+        private readonly string filePath;
+        private readonly string fileName;
+
+        private int maxHS;
+
+
+        public readonly string fileNameFull;
+
+        public List<Score> highScores;
+
+        public HighScoreManager()
+        {
+            appName = "PacMan";
+            filePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                LocalApplicationData), appName);
+
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+
+
+            fileName = "HighScores";
+            fileNameFull = Path.Combine(filePath, fileName);
+
+            maxHS = 10;
+
+            highScores = new List<Score>();
+        }
+
+        public void CreateFile()
+        {
+            if (!File.Exists(fileNameFull))
+                using (FileStream fs = File.Create(fileNameFull)) {}
+
+            ReadHighScores();
+        }
+
+        private void ReadHighScores()
+        {
+            using StreamReader sr = new StreamReader(fileNameFull);
+
+            string s;
+            char separator = '\t';
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] nameAndScore = s.Split(separator);
+                string name = nameAndScore[0];
+
+                int.TryParse(nameAndScore[1], out int score);
+
+                highScores.Add(new Score(name, score));
+            }
+        }
+
+        public void AddHighScore(Score score)
+        {
+            if(IsHighScore(score))
+            {
+                Console.Clear();
+                Console.WriteLine("New HighScore! What should we call you?\n");
+
+                if (score.Name == default) score.InsertName(Console.ReadLine());
+
+                Console.WriteLine($"\nYour score of {score.TotalScore} was " +
+                    $"added to the HighScores!");
+
+                highScores.Add(score);
+                highScores.Sort();
+            }
+        }
+
+        private bool IsHighScore(Score score)
+        {
+            if (highScores.Count < maxHS) return true;
+
+            if (score.TotalScore > highScores[maxHS-1].TotalScore)
+            {
+                highScores.RemoveAt(maxHS - 1);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SaveHighScores()
+        {
+            using (StreamWriter sw = new StreamWriter(fileNameFull))
+                foreach (Score hs in highScores)
+                    sw.WriteLine(hs.Name + "\t" + hs.TotalScore);
+        }
+    }
+}
--- LP2_P2/MainMenu.cs
+++ LP2_P2/MainMenu.cs
@@ -1,11 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace LP2_P2
 {
     public class MainMenu
     {
+        //GameLoop game;
+        HighScoreManager highScoreManager;
 
+        public MainMenu()
+        {
+            //game = new GameLoop();
+            highScoreManager = new HighScoreManager();
+            highScoreManager.CreateFile();
+        }
+
+        public void RunMenu()
+        {
+            Console.WriteLine("1. Play" +
+                            "\n2. High Scores" +
+                            "\nQ. Quit");
+
+            ProcessInput();
+        }
+        private void ProcessInput()
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.D1:
+                    //game.Loop();
+                    //TestHighScores();
+                    break;
+                case ConsoleKey.D2:
+                    ShowHighScores();
+                    break;
+                case ConsoleKey.Q:
+                    Quit();
+                    break;
+            }
+        }
+
+        private void ShowHighScores()
+        {
+            using StreamReader sr = new StreamReader(highScoreManager.fileNameFull);
+
+            string s;
+            char separator = '\t';
+
+            Console.WriteLine($"+++++++++ HighScores +++++++++\n");
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] nameAndScore = s.Split(separator);
+                string name = nameAndScore[0];
+                string score = nameAndScore[1];
+
+                Console.WriteLine($"Player: {name}\tScore: {score, 4}");
+            }
+        }
+
+        private void Quit()
+        {
+            Console.WriteLine("Thank you for playing! Come back any time!");
+            Environment.Exit(0);
+        }
+
+
+        /*
+        private void TestHighScores()
+        {
+            Score s1  = new Score("HUG", 10);
+            Score s2  = new Score("JUS", 25);
+            Score s3  = new Score("ABC", 20);
+            Score s4  = new Score("DEF", 56);
+            Score s5  = new Score("GHI", 78);
+            Score s6  = new Score("JKL", 19);
+            Score s7  = new Score("MNO", 45);
+            Score s8  = new Score("PQR", 33);
+            Score s9  = new Score("STU", 67);
+            Score s10 = new Score("VWX", 96);
+            Score s11 = new Score("YZA", 38);
+            Score s12 = new Score("BCD", 29);
+            Score s13 = new Score("EFG", 12);
+            Score s14 = new Score("HIJ", 74);
+            Score s15 = new Score("HUG", 99);
+
+            highScoreManager.AddHighScore(s1);
+            highScoreManager.AddHighScore(s2);
+            highScoreManager.AddHighScore(s3);
+            highScoreManager.AddHighScore(s4);
+            highScoreManager.AddHighScore(s5);
+            highScoreManager.AddHighScore(s6);
+            highScoreManager.AddHighScore(s7);
+            highScoreManager.AddHighScore(s8);
+            highScoreManager.AddHighScore(s9);
+            highScoreManager.AddHighScore(s10);
+            highScoreManager.AddHighScore(s11);
+            highScoreManager.AddHighScore(s12);
+            highScoreManager.AddHighScore(s13);
+            highScoreManager.AddHighScore(s14);
+            highScoreManager.AddHighScore(s15);
+
+            highScoreManager.SaveHighScores();
+        }*/
     }
 }
