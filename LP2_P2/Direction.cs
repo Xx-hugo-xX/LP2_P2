6d06bbea837863b1a2f81b2f8ff2dc3346b0c01d
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-29 15:04:41 +0000

    Add comments and XML documentation to DoubleBuffer2D class and Direction enumeration

--- LP2_P2/Direction.cs
+++ LP2_P2/Direction.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace LP2_P2
 {
+    /// <summary>
+    /// Enumeration of directions available for movement
+    /// </summary>
     public enum Direction
     {
         Up,
--- LP2_P2/DoubleBuffer2D.cs
+++ LP2_P2/DoubleBuffer2D.cs
@@ -1,28 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LP2_P2
 {
+    /// <summary>
+    /// DoubleBuffer2D of generic type.
+    /// Responsible for containing the current and next frames in bidimensional
+    /// arrays of generic type, being able to swap the next for the current and
+    /// clearing the next frame to be written again.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     class DoubleBuffer2D<T>
     {
+        // Stores the current frame in a generic type bidimensional array
         private T[,] current;
+        // Stores the next frame in a generic type bidimensional array
         private T[,] next;
 
+        // Dimension of frame in X axis
         public int XDim { get => next.GetLength(0); }
+        // Dimension of frame in Y axis
         public int YDim { get => next.GetLength(1); }
 
+        // Indexer to be used in a DoubleBuffer2D instance
         public T this[int x, int y]
         {
             get => current[x, y];
             set => next[x, y] = value;
         }
 
+        // Clears the next frame, to be written over again
         public void Clear()
         {
             Array.Clear(next, 0, XDim * YDim - 1);
         }
 
+        // Class constructor
         public DoubleBuffer2D(int x, int y)
         {
             current = new T[x, y];
@@ -30,6 +42,7 @@ namespace LP2_P2
             Clear();
         }
 
+        // Swaps the current frame for the next frame
         public void Swap()
         {
             T[,] auxNext = next;
