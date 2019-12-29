19bb0f8442f1b364b06bee1726e04afb6940df7e
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-29 17:18:15 +0000

    Add base elements of Ghost class

--- LP2_P2/Ghost.cs
+++ LP2_P2/Ghost.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace LP2_P2
 {
-    public class Ghost
+    public class Ghost : Object
     {
-
+        public Ghost()
+        {
+            Pos = new Position(10, 13);
+            OldPos = new Position(1, 1);
+            Visuals = 'G';
+            BoxCollider = new int[4] { 0, 0, 0 + 1, 0 + 1 };
+        }
     }
 }
